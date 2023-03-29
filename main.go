043aada744fbbea553339a3b2b8f1846b7b6dbd7
Anthony HAMON <anthony@koyeb.com>
2023-03-29 12:59:28 +0200

    Update starting message

--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
 )
 
+const port = 8000
+
 func main() {
-	log.Println("Start server")
+	log.Printf("Start server on port %d", port)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		b, err := json.Marshal(map[string]string{
@@ -21,7 +24,7 @@ func main() {
 		w.Write(b)
 	})
 
-	if err := http.ListenAndServe(":8000", nil); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
 		log.Fatal(err)
 	}
 }
