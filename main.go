4b70622ea663a6bd205ba73e9ad5629eb1278085
Anthony HAMON <anthony@koyeb.com>
2023-04-13 09:04:03 +0200

    Fail the clock

--- main.go
+++ main.go
@@ -1,30 +1,9 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-	"time"
+	"os"
 )
 
-const port = 8000
-
 func main() {
-	log.Printf("Start server on port %d", port)
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		b, err := json.Marshal(map[string]string{
-			"time": time.Now().String(),
-		})
-		if err != nil {
-			log.Fatal(err)
-		}
-		w.WriteHeader(http.StatusOK)
-		w.Write(b)
-	})
-
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
-		log.Fatal(err)
-	}
+	os.Exit(1)
 }
